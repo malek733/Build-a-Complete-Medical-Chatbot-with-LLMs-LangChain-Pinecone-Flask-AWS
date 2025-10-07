85f549b07dde618783c1e318b1685df28bd3ea64
malek733 <malekattia733@gmail.com>
2025-10-07 11:35:39 +0200

    app.py updated

--- app.py
+++ app.py
@@ -67,7 +67,6 @@ def chat():
     return str(response["answer"])
 
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port= 8080, debug= True)

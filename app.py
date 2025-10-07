171a512620866c5496fe7d079c70b0feb82c630e
malek733 <malekattia733@gmail.com>
2025-10-07 11:04:59 +0200

    fixed render port binding

--- app.py
+++ app.py
@@ -67,6 +67,7 @@ def chat():
     return str(response["answer"])
 
 
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
